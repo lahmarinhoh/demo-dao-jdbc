0bbac8bd5eba07c11e6a26cd839cc3551fcdc82c
Lara Marinho Cordeiro <laramaricordeiro@gmail.com>
2024-01-28 21:03:42 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -39,8 +39,7 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public Seller findById(Integer id) {
-		
+	public Seller findById(Integer id) {		
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
@@ -54,20 +53,12 @@ public class SellerDaoJDBC implements SellerDao {
 					);
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			Seller obj = new Seller();
 			if (rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
+				return obj;
 			}
-			return obj;
+			return null;
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
@@ -75,9 +66,27 @@ public class SellerDaoJDBC implements SellerDao {
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		}
-		
+		}	
 	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+	
 
 	@Override
 	public List<Seller> findAll() {
